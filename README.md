1b6211a66c6d1f0b8a1fe570a2a42f8565a2da21
piraces <raul.piraces@gmail.com>
2023-01-20 22:45:10 +0100

    feat: some code improvements

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 [![Deploy to nostr.moe](https://github.com/piraces/nostr.moe/actions/workflows/astro.yml/badge.svg)](https://github.com/piraces/nostr.moe/actions/workflows/astro.yml)
 
 A simple landing page with several nostr projects, clients and some self-hosted projects (in subdomains of nostr.moe).
+
+# Contribute
+
+Everyone can contribute and I appreciate every contribution of any kind that improves this website!
+
+
+# License
+
+MIT
---
+++ src/pages/resources.ts
@@ -0,0 +1,189 @@
+export interface Resource {
+	href: string, 
+	title: string, 
+	body: string
+}
+
+export const ownResources: Resource[] = [
+	{
+		href: "https://nostr.developer.li",
+		title: "nostr.developer.li",
+		body: "A public general purpose relay of confidence."
+	},
+	{
+		href: "https://relay.nostr.moe",
+		title: "relay.nostr.moe",
+		body: "Another public general purpose relay of confidence."
+	},
+	{
+		href: "https://rsslay.nostr.moe",
+		title: "rsslay",
+		body: "Turns RSS or Atom feeds into Nostr profiles."
+	},
+	{
+		href: "https://rssbridge.nostr.moe",
+		title: "RSS-Bridge",
+		body: "RSS and Atom feed generator for websites that don't have one."
+	},
+	{
+		href: "https://foolay.nostr.moe",
+		title: "foolay",
+		body: "A minimal, forward-only Nostr relay."
+	}
+];
+
+export const projects: Resource[] = [
+	{
+		href: "https://www.nostr.net",
+		title: "Awesome nostr",
+		body: "A curated list of nostr projects and resources."
+	},
+	{
+		href: "https://nostr.how",
+		title: "nostr.how",
+		body: "Nostr: What, why, and how?"
+	},
+	{
+		href: "https://nostr.band/",
+		title: "nostr.band",
+		body: "Your Web portal into the Nostr universe."
+	},
+	{
+		href: "https://nostr.watch/",
+		title: "nostr.watch",
+		body: "Real-time checking of status of some known relays with advanced checks."
+	},
+	{
+		href: "https://nostr.guru/",
+		title: "Nostr Gateway",
+		body: "A nostr web gateway for viewing events by their ID."
+	},
+	{
+		href: "https://nostr.directory/",
+		title: "Nostr Directory",
+		body: "Nostr Public Key Database. Find your follows on other social media in nostr."
+	},
+	{
+		href: "https://nashboard.space/",
+		title: "Nashboard",
+		body: "A Nostr network dashboard."
+	},
+	{
+		href: "https://nosbin.com/",
+		title: "Nosbin",
+		body: "The decentralized pasting platform, built on nostr."
+	},
+	{
+		href: "https://sendstr.com/",
+		title: "Sendstr",
+		body: "E2E encrypted shared clipboard."
+	},
+	{
+		href: "https://jesterui.github.io/",
+		title: "Jester",
+		body: "Chess over nostr."
+	},
+	{
+		href: "https://notes.blockcore.net/",
+		title: "Blockcore",
+		body: "Organize following in circles and have both public and private following lists."
+	},
+	{
+		href: "https://nostrum.pro/",
+		title: "Nostrum",
+		body: "Search engine and stats"
+	},
+	{
+		href: "https://brb.io/",
+		title: "brb.io",
+		body: "Explore Nostr (experiment)"
+	},
+	{
+		href: "https://nostr-resources.com/",
+		title: "Nostr resources",
+		body: "A collection of nostr resources and information"
+	},
+	{
+		href: "https://nostrview.com/",
+		title: "Nostr view",
+		body: "Search engine"
+	}
+];
+
+export const clients: Resource[] = [
+	{
+		href: "https://snort.social/",
+		title: "snort.social",
+		body: "Web client"
+	},
+	{
+		href: "https://astral.ninja/",
+		title: "astral.ninja",
+		body: "Web client"
+	},
+	{
+		href: "https://damus.io/",
+		title: "Damus",
+		body: "iOS client"
+	},
+	{
+		href: "https://monstr.app/",
+		title: "Monstr",
+		body: "Truly Private Messaging for web, iOS and Android"
+	},
+	{
+		href: "https://github.com/KoalaSat/nostros",
+		title: "Nostros",
+		body: "A Nostr mobile client"
+	},
+	{
+		href: "https://coracle.social/",
+		title: "Coracle",
+		body: "Web client"
+	},
+	{
+		href: "https://iris.to/",
+		title: "Iris",
+		body: "Web client"
+	},
+	{
+		href: "https://hamstr.to/",
+		title: "Hamstr",
+		body: "Web client"
+	},
+	{
+		href: "https://nostr.rocks/",
+		title: "Nostr.rocks",
+		body: "Web client"
+	},
+	{
+		href: "https://www.minds.com/",
+		title: "Minds",
+		body: "Open source social network with nostr support"
+	},
+	{
+		href: "https://yosup.app/",
+		title: "Yo Sup",
+		body: "Web client"
+	},
+	{
+		href: "https://github.com/KotlinGeekDev/Nosky",
+		title: "Nosky",
+		body: "A native Android client for Nostr"
+	},
+	{
+		href: "https://github.com/kaiwolfram/Nozzle",
+		title: "Nozzle",
+		body: "A Twitter-like native Android client"
+	},
+	{
+		href: "https://web.nostrid.app/",
+		title: "Nostrid",
+		body: "Multi-platform client"
+	},
+    {
+        href: "https://nostr.pro/",
+        title: "Nostr pro",
+        body: "Web client"
+    }
+];
